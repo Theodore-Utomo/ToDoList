6a39b0417d0c6bba016e3e983ca76a3f215eddd6
TheodoreUtomo <theodoreutomo@gmail.com>
2024-10-09 13:11:15 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Theodore Utomo on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
