19fdbc4d80138c53b171acfff6715804e876aa24
TheodoreUtomo <theodoreutomo@gmail.com>
2024-10-15 01:18:50 -0400

    Added Swift Data and Delete

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -6,30 +6,82 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct DetailView: View {
-    var passedValue: String // Don't initialize it - it will be passed from parent view
     @Environment(\.dismiss) private var dismiss //Dismisses a screen
+    @Environment(\.modelContext) var modelContext
+    
+    @State var toDo: ToDo
+    @State private var item = ""
+    @State private var reminderIsOn = false
+    @State private var dueDate = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
+    @State private var notes = ""
+    @State private var isCompleted = false
+    
     var body: some View {
-        VStack {
-            Spacer()
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            
-            Text("You Are a Swifty Legend!\nAnd you passed over the value: \(passedValue)")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            Spacer()
-            Button("Get Back!") {
-                dismiss() //Dismisses a screen
+        List {
+            TextField("Enter To Do here", text: $item)
+                .font(.title)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+                .listRowSeparator(.hidden)
+            Toggle("Set Reminder:", isOn: $reminderIsOn)
+                .listRowSeparator(.hidden)
+
+            DatePicker("Date:", selection: $dueDate)
+                .listRowSeparator(.hidden)
+                .disabled(!reminderIsOn)
+
+            Text("Notes: ")
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            TextField("Notes: ", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            Toggle("Completed: ", isOn: $isCompleted)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+        .onAppear() {
+            item = toDo.item
+            reminderIsOn = toDo.reminderIsOn
+            dueDate = toDo.dueDate
+            notes = toDo.notes
+            isCompleted = toDo.isCompleted
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    //Move data from local vars to toDo object
+                    toDo.item = item
+                    toDo.reminderIsOn = reminderIsOn
+                    toDo.dueDate = dueDate
+                    toDo.notes = notes
+                    toDo.isCompleted = isCompleted
+                    modelContext.insert(toDo)
+                    guard let _ = try? modelContext.save() else {
+                        print("Save on DetailView did not work!")
+                        return
+                    }
+                    dismiss()
+                }
             }
         }
-        .padding()
     }
 }
 
 #Preview {
-    DetailView(passedValue: "")
+    NavigationStack {
+        DetailView(toDo: ToDo())
+            .modelContainer(for: ToDo.self, inMemory: true)
+    }
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -6,27 +6,79 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ToDoListView: View {
-    var toDos = ["Learn Swift", "Build Apps", "Change The World", "Bring the Awesome", "Take a Vacation"]
+    @State private var sheetIsPresented = false
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var toDos: [ToDo]
+    
     var body: some View {
         NavigationStack {
             List {
-                ForEach(toDos, id: \.self) { toDo in
-                    NavigationLink {
-                        DetailView(passedValue: toDo)
-                    } label: {
-                        Text(toDo)
+                ForEach(toDos) { toDo in
+                    HStack {
+                        Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
+                            .onTapGesture {
+                                toDo.isCompleted.toggle()
+                                guard let _ = try? modelContext.save() else {
+                                    print("Error: Save after .toggle on ToDoListView did not work")
+                                    return
+                                }
+                            }
+                        NavigationLink {
+                            DetailView(toDo: toDo)
+                        } label: {
+                            Text(toDo.item)
+                        }
+                        .font(.title2)
+                        .swipeActions {
+                            Button("Delete", role: .destructive) {
+                                modelContext.delete(toDo)
+                                guard let _ = try? modelContext.save() else {
+                                    print("Error: Could not save changes after delete")
+                                    return
+                                }
+                            }
+                        }
                     }
+                    .font(.title2)
                 }
+//                .onDelete { indexSet in
+//                    indexSet.forEach({modelContext.delete(toDos[$0])})
+//                    guard let _ = try? modelContext.save() else {
+//                        print("Error: Could not save changes after delete")
+//                        return
+//                    }
+//                }
             }
             .navigationTitle("To Do List")
             .navigationBarTitleDisplayMode(.automatic)
             .listStyle(.plain)
+            .sheet(isPresented: $sheetIsPresented) {
+                NavigationStack{
+                    DetailView(toDo: ToDo())
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    
+                }
+            }
         }
     }
 }
 
 #Preview {
-    ToDoListView()
+    NavigationStack {
+        ToDoListView()
+            .modelContainer(for: ToDo.self, inMemory: true)
+    }
 }
